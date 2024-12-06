bbfecdfedb757c462062de5ead202a4b0e102942
ayshafa <121549852+ayshafa@users.noreply.github.com>
2024-12-06 15:48:31 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogPostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [BlogPostController::class, 'index'])->name('blog-posts');
 Route::resource('blog-posts', BlogPostController::class);
